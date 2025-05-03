02839303ecbcdb2d70ca5b62a67b289a7aefce02
A.YASIRU NADEESHA ATHTHANAYAKA <yasiru.20232389@iit.ac.lk>
2025-05-04 00:47:50 +0530

    Initialize the Spring boot project

---
+++ BACKEND/Social_Media_Application/src/main/java/backend/social_media_application/SocialMediaApplication.java
@@ -0,0 +1,13 @@
+package backend.social_media_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialMediaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocialMediaApplication.class, args);
+    }
+
+}
