e16c2d0f6b2c3f0e0e25afa817b7e3890cee033a
A . Yasiru Nadeesha Aththanayaka <yasiru.20232389@iit.ac.lk>
2025-06-11 22:27:12 +0530

    Create auth response

---
+++ BACKEND/Social_Media_Application/src/main/java/backend/social_media_application/dto/AuthResponse.java
@@ -0,0 +1,18 @@
+package backend.social_media_application.dto;
+
+public class AuthResponse {
+    private String token;
+    private UserDTO user;
+
+    public AuthResponse(String token, UserDTO user) {
+        this.token = token;
+        this.user = user;
+    }
+
+    // Getters and Setters
+    public String getToken() { return token; }
+    public void setToken(String token) { this.token = token; }
+
+    public UserDTO getUser() { return user; }
+    public void setUser(UserDTO user) { this.user = user; }
+}
